cc0836be802034b8176c94eb215af4617c1ac54f
moondalnim <96918965+moondalnim@users.noreply.github.com>
2022-01-21 11:52:13 +0900

    Create 부족한 금액 계산하기.swift

---
+++ Programmers/부족한 금액 계산하기.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+func solution(_ price: Int, _ money: Int, _ count: Int) -> Int64{
+    let answer = price * count * (count + 1) / 2 - money
+
+    return answer <= 0 ? .zero : Int64(answer)
+}
