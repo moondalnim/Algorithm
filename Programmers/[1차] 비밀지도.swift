47d5859448cf05afe2d1562189187172d2e63cdb
moondalnim <96918965+moondalnim@users.noreply.github.com>
2022-01-22 23:52:57 +0900

    Update [1차] 비밀지도.swift

--- Programmers/[1차] 비밀지도.swift
+++ Programmers/[1차] 비밀지도.swift
@@ -1,13 +1,8 @@
 func solution(_ n: Int, _ arr1: [Int], _ arr2: [Int]) -> [String] {
-    var answer: [String] = []
+    return (0..<n).map {
+        var binary = String(arr1[$0] | arr2[$0], radix: 2)
+        binary = String(repeating: "0", count: n - binary.count) + binary
 
-    for i in 0..<n {
-        let result = String(arr1[i] | arr2[i], radix: 2)
-            .replacingOccurrences(of: "1", with: "#")
-            .replacingOccurrences(of: "0", with: " ")
-
-        answer.append(String(repeating: " ", count: n - result.count) + result)
+        return binary.reduce("") { $0 + ($1 == "0" ? " " : "#") }
     }
-
-    return answer
 }
