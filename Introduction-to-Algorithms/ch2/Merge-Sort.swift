b19b474f535dd31443ab27f9423d9305df0aa8d7
moondalnim <96918965+moondalnim@users.noreply.github.com>
2022-02-09 21:57:57 +0900

    Update Merge-Sort.swift

--- Introduction-to-Algorithms/ch2/Merge-Sort.swift
+++ Introduction-to-Algorithms/ch2/Merge-Sort.swift
@@ -1,14 +1,38 @@
-func insertionSort(_ array: inout [Int]) -> [Int] {
-    for keyIndex in 1..<array.count {
-        let key = array[keyIndex]
-        var index = keyIndex - 1
+func merge(_ array: inout [Int], _ start: Int, _ mid: Int, _ end: Int) -> Void {
+    let leftCount = mid - start + 1
+    let rightCount = end - mid
 
-        while index >= 0, key < array[index] {
-            array[index + 1] = array[index]
-            index -= 1
-        }
-        array[index + 1] = key
+    var left = Array(repeating: 0, count: leftCount + 1)
+    var right = Array(repeating: 0, count: rightCount + 1)
+
+    for leftIndex in 0..<leftCount {
+        left[leftIndex] = array[start + leftIndex]
+    }
+    for rightIndex in 0..<rightCount {
+        right[rightIndex] = array[mid + 1 + rightIndex]
     }
 
-    return array
+    left[leftCount] = .max
+    right[rightCount] = .max
+
+    var leftIndex = 0
+    var rightIndex = 0
+    for index in start...end {
+        if left[leftIndex] <= right[rightIndex] {
+            array[index] = left[leftIndex]
+            leftIndex += 1
+        } else {
+            array[index] = right[rightIndex]
+            rightIndex += 1
+        }
+    }
+}
+
+func mergeSort(_ array: inout [Int], _ start: Int, _ end: Int) {
+    if start < end {
+        let mid = (start + end) / 2
+        mergeSort(&array, start, mid)
+        mergeSort(&array, mid + 1, end)
+        merge(&array, start, mid, end)
+    }
 }
