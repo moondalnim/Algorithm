9ff3357c9cfa580e28b82ed2b15d9f7a7bdd3468
moondalnim <96918965+moondalnim@users.noreply.github.com>
2022-02-09 21:41:37 +0900

    Create Quick-Sort.swift

---
+++ Introduction-to-Algorithms/ch7/Quick-Sort.swift
@@ -0,0 +1,23 @@
+func quickSort(_ array: inout [Int], _ low: Int, _ high: Int) {
+    if low < high {
+        let pivot = partition(&array, low, high)
+        quickSort(&array, low, pivot - 1)
+        quickSort(&array, pivot + 1, high)
+    }
+}
+
+func partition(_ array: inout [Int], _ low: Int, _ high: Int) -> Int {
+    let pivot = array[high]
+
+    var i = low - 1
+    for j in low..<high {
+        if array[j] <= pivot {
+            i += 1
+            array.swapAt(i, j)
+        }
+    }
+
+    array.swapAt(i + 1, high)
+
+    return i + 1
+}
