0a449e080335baad41a81fc31187f5f6b97f5abd
moondalnim <96918965+moondalnim@users.noreply.github.com>
2022-02-18 23:52:37 +0900

    Create Direct-Address-Table.swift

---
+++ Introduction-to-Algorithms/ch11/Direct-Address-Table.swift
@@ -0,0 +1,38 @@
+struct K {
+    static let maxTableSize = 10
+}
+
+struct Table {
+    private var bucket: [Node<Int>?] = Array(repeating: nil, count: K.maxTableSize)
+
+    subscript(key: Int) -> Node<Int>? {
+        get {
+            self.bucket[key]
+        }
+        set {
+            self.bucket[key] = newValue
+        }
+    }
+}
+
+class Node<Key: Hashable> {
+    var key: Key
+    var value: Any?
+
+    init(_ key: Key, _ value: Any? = nil) {
+        self.key = key
+        self.value = value
+    }
+}
+
+func directAddressSearch(_ table: Table, _ key: Int) -> Node<Int>? {
+    return table[key]
+}
+
+func directAddressInsert(_ table: inout Table, _ node: Node<Int>) {
+    table[node.key] = node
+}
+
+func directAddressDelete(_ table: inout Table, _ node: Node<Int>) {
+    table[node.key] = nil
+}
