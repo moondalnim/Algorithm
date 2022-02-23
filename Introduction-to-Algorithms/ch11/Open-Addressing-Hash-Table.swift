c69070057bd6cd3c32b40190c40ca382f6e451b2
moondalnim <96918965+moondalnim@users.noreply.github.com>
2022-02-23 23:48:37 +0900

    Update Open-Addressing-Hash-Table.swift

--- Introduction-to-Algorithms/ch11/Open-Addressing-Hash-Table.swift
+++ Introduction-to-Algorithms/ch11/Open-Addressing-Hash-Table.swift
@@ -1,19 +1,23 @@
 struct K {
-    static let hashTableSizePreferredPrimeNumber = 701
+    static let hashTableSize = 1024
     static let deleted = -1
 }
 
-extension Int {
-    var deleted: Int {
-        return -1
-    }
+func hashUsingDivision(_ key: Int) -> Int {
+    return key % K.hashTableSize
+}
+
+func hashUsingMultiplication(_ key: Int) -> Int {
+    let knuth = 0.6180339887
+
+    return Int(Double(K.hashTableSize) * (Double(key) * knuth.truncatingRemainder(dividingBy: 1)))
 }
 
 struct HashTable {
     private var bucket: [Int?]
 
     init() {
-        self.bucket = Array(repeating: nil, count: K.hashTableSizePreferredPrimeNumber)
+        self.bucket = Array(repeating: nil, count: K.hashTableSize)
     }
 
     subscript(hashValue: Int) -> Int? {
@@ -26,17 +30,23 @@ struct HashTable {
     }
 }
 
-func hash(_ key: Int, _ index: Int) -> Int {
-    return (hashDivisionMethod(key) + index) % K.hashTableSizePreferredPrimeNumber
+func hashUsingLinearProbing(_ key: Int, _ index: Int) -> Int {
+    return (hashUsingDivision(key) + index) % K.hashTableSize
 }
 
-func hashDivisionMethod(_ key: Int) -> Int {
-    return key % K.hashTableSizePreferredPrimeNumber
+func hashUsingQuadraticProbing(_ key: Int, _ index: Int) -> Int {
+    let const1 = 1 / 2, const2 = 1 / 2
+
+    return (hashUsingMultiplication(key) + const1 * index + const2 * index * index) % K.hashTableSize
+}
+
+func hashUsingDoubleHashing(_ key: Int, _ index: Int) -> Int {
+    return (hashUsingDivision(key) + index * hashUsingMultiplication(key)) % K.hashTableSize
 }
 
 func hashInsert(_ hashTable: inout HashTable, _ key: Int) -> Int {
-    for index in 0...K.hashTableSizePreferredPrimeNumber {
-        let hashValue = hash(key, index)
+    for index in 0...K.hashTableSize {
+        let hashValue = hashUsingDoubleHashing(key, index)
 
         if hashTable[hashValue] == nil || hashTable[hashValue] == K.deleted {
             hashTable[hashValue] = key
@@ -49,8 +59,8 @@ func hashInsert(_ hashTable: inout HashTable, _ key: Int) -> Int {
 }
 
 func hashSearch(_ hashTable: HashTable, _ key: Int) -> Int? {
-    for index in 0...K.hashTableSizePreferredPrimeNumber {
-        let hashValue = hash(key, index)
+    for index in 0...K.hashTableSize {
+        let hashValue = hashUsingDoubleHashing(key, index)
 
         if hashTable[hashValue] == nil {
             return nil
@@ -63,8 +73,8 @@ func hashSearch(_ hashTable: HashTable, _ key: Int) -> Int? {
 }
 
 func hashDelete(_ hashTable: inout HashTable, _ key: Int) {
-    for index in 0...K.hashTableSizePreferredPrimeNumber {
-        let hashValue = hash(key, index)
+    for index in 0...K.hashTableSize {
+        let hashValue = hashUsingDoubleHashing(key, index)
 
         if hashTable[hashValue] == key {
             hashTable[hashValue] = K.deleted
@@ -73,3 +83,4 @@ func hashDelete(_ hashTable: inout HashTable, _ key: Int) {
         }
     }
 }
+
