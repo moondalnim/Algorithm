f5ef722dacd73c76cd5254795648edb3a835ada1
moondalnim <96918965+moondalnim@users.noreply.github.com>
2022-02-25 23:49:44 +0900

    Create Binary-Tree-Walk.swift

---
+++ Introduction-to-Algorithms/ch12/Binary-Tree-Walk.swift
@@ -0,0 +1,38 @@
+struct BinaryTree<Key: Comparable> {
+    var root: Node<Key>?
+}
+
+class Node<Key: Comparable> {
+    var key: Key
+    var parent: Node?
+    var left: Node?
+    var right: Node?
+
+    required init(_ key: Key) {
+        self.key = key
+    }
+}
+
+func inorderTreeWalk<Key: Comparable>(_ root: Node<Key>?) {
+    if root != nil {
+        inorderTreeWalk(root?.left)
+        print(root?.key as Any)
+        inorderTreeWalk(root?.right)
+    }
+}
+
+func preorderTreeWalk<Key: Comparable>(_ root: Node<Key>?) {
+    if root != nil {
+        print(root?.key as Any)
+        preorderTreeWalk(root?.left)
+        preorderTreeWalk(root?.right)
+    }
+}
+
+func postorderTreeWalk<Key: Comparable>(_ root: Node<Key>?) {
+    if root != nil {
+        postorderTreeWalk(root?.left)
+        postorderTreeWalk(root?.right)
+        print(root?.key as Any)
+    }
+}
